57d2dc490ee2b43c1d4eb37e3468be379e784677
denfushi <awadomar@gmail.com>
2023-06-14 15:32:59 -0700

    Variables

---
+++ Variables.py
@@ -0,0 +1,34 @@
+# print('STRINGS')
+# print('--------------------')
+# print('this is a string')
+# print('each letter and space is an ASCII character in a cell of memory')
+# print('in other words a string is a sequence of ASCII characters')
+# print('each characters position in the sequence is called an index')
+# print('the first index is 0')
+# print('this is a string indexed out is 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15')
+# print('every letter or character is in a cell of memory from 0 to 15')
+# print('INTEGERS & FLOATS')
+# print('--------------------')
+# print('integers are whole numbers postive and negative')
+# print('floats are numbers with a decimal point positive and negative')
+# print('BOOLEANS')
+# print('--------------------')
+# print('booleans are True or False')
+
+# Integers and Strings
+# num1 = 3
+# num2 = 2
+
+# result = num1 + num2
+# print('result is '+ str(result))
+
+# Floats and Strings
+num1 = 5.5
+num2 = 4.4
+
+result = num1 + num2
+print("result is " + str(result))
+
+# Booleans
+open = True
+closed = False
