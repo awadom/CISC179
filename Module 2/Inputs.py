1dcd54cb0657e88ef67e49ce0e8a75972b13e4b6
denfushi <awadomar@gmail.com>
2023-06-14 20:27:03 -0700

    Inputs

---
+++ Module 2/Inputs.py
@@ -0,0 +1,17 @@
+month_conversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+month = input('Enter a month: ' + '\n')
+print(month_conversions.get(month, "Not found"))
