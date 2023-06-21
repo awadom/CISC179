db8dbaca64b01ff038f00dbccdb3ca9d44336328
denfushi <awadomar@gmail.com>
2023-06-20 23:03:50 -0700

    Mara update

--- Module 3/Module 3 Assignment.py
+++ Module 3/Module 3 Assignment.py
@@ -1,21 +1,19 @@
 # Function to print out patient info for those that are checked in
-def count_checked_in_patients(patient_data):
-    checked_in_count = 0
-
-    for patient in patient_data:
-        name = patient[0]
-        patient_number = patient[1]
-        room_number = patient[2]
-        checked_in = patient[3]
-
-        if checked_in == "Yes":
-            checked_in_count += 1
-            print("Patient Name:", name)
-            print("Patient Number:", patient_number)
-            print("Room Number:", room_number)
-            print()
-
-    return checked_in_count
+def check_in_count(data):
+    checkin = 0
+    for patient in data:
+        if patient[3] == "Yes":
+            checkin += 1
+            print(
+                "Patient Name: "
+                + patient[0]
+                + "\nPatient Number: "
+                + str(patient[1])
+                + "\nRoom Number: "
+                + str(patient[2])
+                + "\n--------"
+            )
+    return checkin
 
 
 # Patient data from the table provided in the assignment
@@ -30,5 +28,25 @@ patient_data = [
 ]
 
 # Run the function and get the total patients
-checked_in_patients = count_checked_in_patients(patient_data)
-print("Total Checked-In Patients:", checked_in_patients)
+print("Check in count: ", check_in_count(patient_data))
+
+# def count_checked_in_patients(patient_data):
+#     checked_in_count = 0
+
+#     for patient in patient_data:
+#         name = patient[0]
+#         patient_number = patient[1]
+#         room_number = patient[2]
+#         checked_in = patient[3]
+
+#         if checked_in == "Yes":
+#             checked_in_count += 1
+#             print("Patient Name:", name)
+#             print("Patient Number:", patient_number)
+#             print("Room Number:", room_number)
+#             print()
+
+#     return checked_in_count
+
+# checked_in_patients = count_checked_in_patients(patient_data)
+# print("Total Checked-In Patients:", checked_in_patients)
